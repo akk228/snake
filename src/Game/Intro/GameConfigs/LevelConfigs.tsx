2369e17aad2729cf447a636d04d59f7adba9bee1
Andrei Kovtun <andreykpq@gmail.com>
2025-01-12 19:24:16 +0100

    little stzle correction

--- src/Game/Intro/GameConfigs/LevelConfigs.tsx
+++ src/Game/Intro/GameConfigs/LevelConfigs.tsx
@@ -13,9 +13,9 @@ export function LevelConfigs(props: ILevelConfigProps): JSX.Element {
         const levels: Level[] = [Level.Easy, Level.Medium, Level.FuckHard];
 
         return levels.map((level: Level): React.ReactElement => (
-            <div key={level}>
-                <label
-                    htmlFor={`level-${level}`}>{Level[level]}
+            <div key={`difficultyLevel-${level}`}>
+                <label htmlFor={`level-${level}`}>
+                    <code>{Level[level]}</code>
                 </label>
                 <input
                     type="radio"

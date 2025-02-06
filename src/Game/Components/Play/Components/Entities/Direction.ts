4a988e0eac8a30facc78d35bcd476ec5ae9e4f64
Andrei Kovtun <andreykpq@gmail.com>
2025-02-06 19:02:07 +0100

    UPdates direction Dictionary

--- src/Game/Components/Play/Components/Entities/Direction.ts
+++ src/Game/Components/Play/Components/Entities/Direction.ts
@@ -1,6 +1,15 @@
-export const Direction = {
-    Up: [-1, 0],
-    Down: [1, 0],
-    Left: [0, -1],
-    Right: [0, 1]
+import { Level } from "../../../../Entities/Enums/Level";
+
+export enum Direction {
+    Up = "Up",
+    Down = "Down",
+    Left = "Left",
+    Right = "Right"
+}
+
+export const Directions = {
+    [Direction[Direction.Up]]: [-1, 0],
+    [Direction[Direction.Down]]: [1, 0],
+    [Direction[Direction.Left]]: [0, -1],
+    [Direction[Direction.Right]]: [0, 1]
 }

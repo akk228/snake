2d9692a75981fa841b90e899538f8ca203d752c1
Andrei Kovtun <andreykpq@gmail.com>
2025-02-06 19:04:51 +0100

    Updates direction to be a record

--- src/Game/Components/Play/Components/Entities/Direction.ts
+++ src/Game/Components/Play/Components/Entities/Direction.ts
@@ -7,9 +7,9 @@ export enum Direction {
     Right = "Right"
 }
 
-export const Directions = {
-    [Direction[Direction.Up]]: [-1, 0],
-    [Direction[Direction.Down]]: [1, 0],
-    [Direction[Direction.Left]]: [0, -1],
-    [Direction[Direction.Right]]: [0, 1]
+export const Directions: Record<Direction, number[]> = {
+    [Direction.Up]: [-1, 0],
+    [Direction.Down]: [1, 0],
+    [Direction.Left]: [0, -1],
+    [Direction.Right]: [0, 1]
 }
